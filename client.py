c195b7e494bd99e55cea945bcd300b220463de2a
SakeXYZ <islamshamshiev2002@gmail.com>
2024-09-26 11:59:15 +0600

    do for pullrequests

--- client.py
+++ client.py
@@ -1,44 +1,11 @@
 import socket
-import threading
 
-HOST = '0.0.0.0'
+HOST = "127.0.0.1"
 PORT = 12345
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello server")
+    data = s.recv(1024)
 
-def received_message(client_socket):
-    while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                break
-            print(message)
-        except:
-            break
-
-
-def send_message(client_socket):
-    while True:
-        try:
-            message = input("")
-            if message.lower() == '/quit':
-                client_socket.close()
-                break
-            client_socket.send(message.encode('utf-8'))
-        except:
-            break
-
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
-
-name = input("Enter your name: ")
-client_socket.send(f"Name {name} has been joined the chat".encode('utf-8'))
-
-received_thread = threading.Thread(target=received_message, args=(client_socket,))
-send_thread = threading.Thread(target=send_message, args=(client_socket,))
-
-received_thread.start()
-send_thread.start()
-
-received_thread.join()
-send_thread.join()
+print(f"Received {data}")
--- server.py
+++ server.py
@@ -1,34 +1,22 @@
 import socket
-import threading
 
-HOST = '0.0.0.0'
+HOST = "127.0.0.1"
 PORT = 12345
 
 
-def handle_client(client_socket, clients):
-    while True:
-        try:
-            message = client_socket.recv(1024)
-            if not message:
+def new_client(connection: socket.socket) -> None:
+    with connection:
+        while True:
+            data = connection.recv(1024)
+            if not data:
                 break
+            print(f"Received {data}")
+            connection.sendall(b"Hello client")
 
-            for client in clients:
-                if client != client_socket:
-                    client.send(message)
-        except:
-            break
-    client_socket.close()
-    clients.remove(client_socket)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen(5)
-
-clients = set()
-
-while True:
-    clients_socket, addr = server_socket.accept()
-    print('')
-    clients.add(clients_socket)
-    clients_thread = threading.Thread(target=handle_client, args=(clients_socket, clients))
-    clients_thread.start()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, _ = s.accept()
+        new_client(conn)
